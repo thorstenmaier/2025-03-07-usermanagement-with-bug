0379c115ba532e88cb283d4472ab93183d1afc47
Thorsten Maier <thorsten.a.maier@accenture.com>
2025-03-10 10:02:15 +0100

    Fix tests

--- src/test/java/com/accenture/spring/user/service/UserServiceTest.java
+++ src/test/java/com/accenture/spring/user/service/UserServiceTest.java
@@ -1,14 +1,11 @@
 package com.accenture.spring.user.service;
 
 import com.accenture.spring.user.domain.User;
-import com.accenture.spring.user.repository.LogEntryRepository;
-import com.accenture.spring.user.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -23,6 +20,7 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     public void testGetAllUsers() {
         List<User> users = userService.getAllUsers();
         assertEquals(3, users.size());
